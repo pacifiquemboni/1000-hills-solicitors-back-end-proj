9e2ce0db6403b0c21b6b28c27750da29de30cc0f
pacifiquemboni <pacifiquemboni@gmail.com>
2024-05-29 14:45:33 +0200

    first deployed version

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,269 +1,345 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import express from "express";
+import bodyParser from "body-parser";
 
 import nodemailer from "nodemailer";
 import jwt from "jsonwebtoken";
 import Users from "../model/userModel.js";
-import verifyEmail from "../varidation/verify.js"
-import { JWT } from '../helper/jwt.js';
-import User from '../model/userModel.js';
-import bcrypt from 'bcrypt';
+import verifyEmail from "../varidation/verify.js";
+import { JWT } from "../helper/jwt.js";
+import User from "../model/userModel.js";
+import bcrypt from "bcrypt";
 //  Assuming the model is defined as 'User'
 
 // Function to send verification email
-async function sendVerificationEmail(email,token) {
-  const transporter = nodemailer.createTransport({ 
-    service: 'gmail', 
-    auth: { 
-        user: process.env.EMAIL_USERNAME, 
-        pass: process.env.EMAIL_PASSWORD 
-    } 
-}); 
-  
-   
-  
-const mailConfigurations = { 
-  
-    // It should be a string of sender/server email 
-    from: 'EMAIL_USERNAME', 
-  
-    to: email, 
-  
-    // Subject of Email 
-    subject: 'Email Verification', 
-      
-    // This would be the text of email body 
+async function sendVerificationEmail(email, token) {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  const mailConfigurations = {
+    // It should be a string of sender/server email
+    from: "EMAIL_USERNAME",
+
+    to: email,
+
+    // Subject of Email
+    subject: "Email Verification",
+
+    // This would be the text of email body
     html: `<p>Hi!  ${email},</p>
     <p>You have recently visited our website and entered your email.</p>
     <p>Please follow the given link to verify your email:</p>
     <h3><a href="http://127.0.0.1:5501/verify.html?token=${token}?email" target="blank_">here</a></h3>
-    <p>Thanks</p>`
-      
-}; 
-  
-transporter.sendMail(mailConfigurations, function(error, info){ 
-    // if (error) throw Error(error); 
-    console.log('Email Sent Successfully'); 
-    
-}); 
+    <p>Thanks</p>`,
+  };
+
+  transporter.sendMail(mailConfigurations, function (error, info) {
+    // if (error) throw Error(error);
+    console.log("Email Sent Successfully");
+  });
 }
 
 class UsersController {
   static async registerUsers(req, res) {
     try {
-      const { idNo, email , firstName, lastName, password,role,phoneNumber,preferredContact,district,sector,cell} = req.body;
-      
+      const {
+        idNo,
+        email,
+        firstName,
+        lastName,
+        password,
+        role,
+        phoneNumber,
+        preferredContact,
+        district,
+        sector,
+        cell,
+      } = req.body;
+
       // Check if user with the email already exists
       const existingUser = await Users.findOne({ email });
 
       if (existingUser) {
-        return res.status(409).json({ message: "Email already exists" });
+        return res
+          .status(409)
+          .json({ status: "fail", message: "Email already exists" });
       }
-      if(!verifyEmail.verifyId(idNo)){
-        return res.status(400).json({ message: "Your national identity card number should have 16 characters." });
+      if (!verifyEmail.verifyId(idNo)) {
+        return res.status(400).json({
+          status: "fail",
+          message:
+            "Your national identity card number should have 16 characters.",
+        });
       }
-      if(!verifyEmail.validateEmail(email)){
-        return res.status(400).json({ message: "Invalid email address. Please enter a valid email." });
+      if (!verifyEmail.validateEmail(email)) {
+        return res.status(400).json({
+          status: "fail",
+          message: "Invalid email address. Please enter a valid email.",
+        });
       }
-      if(!verifyEmail.verifyStrongPassword(password)){
-        return res.status(400).json({ message: "Password not strong enough. Your password should contain at least one uppercase letter, one lowercase letter, one digit, one special character, and should be between 8 and 16 characters long." });
+      if (!verifyEmail.verifyStrongPassword(password)) {
+        return res.status(400).json({
+          status: "fail",
+          message:
+            "Password not strong enough. Your password should contain at least one uppercase letter, one lowercase letter, one digit, one special character, and should be between 8 and 16 characters long.",
+        });
       }
-      
+      if (
+        !idNo ||
+        !email ||
+        !firstName ||
+        !lastName ||
+        !password ||
+        !phoneNumber ||
+        !preferredContact ||
+        !district ||
+        !sector ||
+        !cell
+      ) {
+        return res.status(400).json({
+          status: "fail",
+          message: "All fields are required",
+        });
+      }
       // Generate token for email verification
-      const token = jwt.sign({ email, idNo, role }, process.env.JWT_SECRET, { expiresIn: '1d' }); // Token expires in 1 day
+      const token = jwt.sign({ email, idNo, role }, process.env.JWT_SECRET, {
+        expiresIn: "1d",
+      }); // Token expires in 1 day
 
       // Send verification email with token
-    
-      await sendVerificationEmail(email, token);
-     const registerUser = await Users.create({
-      idNo, email , firstName, lastName, password,role,phoneNumber,preferredContact,district,sector,cell
-     }) 
 
-      return res.status(200).json({ 
+      await sendVerificationEmail(email, token);
+      // const salt = bcrypt.genSaltSync(12)
+      // const hashPassword = bcrypt.hashSync(password, 10);
+      const registerUser = await Users.create({
+        idNo,
+        email,
+        firstName,
+        lastName,
+        password,
+        role,
+        phoneNumber,
+        preferredContact,
+        district,
+        sector,
+        cell,
+      });
+
+      return res.status(200).json({
+        status: "success",
         message: "Verification email sent",
-        data:registerUser
-       });
+        data: registerUser,
+      });
     } catch (error) {
       console.error("Error registering user:", error);
-      res.status(500).json({ error: "Failed to register user" });
+      res.status(500).json({
+        status: "fail",
+        error: "Failed to register user",
+      });
     }
   }
 
-  static async getUsers(req,res){
-    try{
-      const allUsers= await Users.find();
+  static async getUsers(req, res) {
+    try {
+      const allUsers = await Users.find();
       return res.status(200).json({ data: allUsers, message: "All users" });
-    }
-    catch(error){
+    } catch (error) {
       console.log(error);
       return res.status(500).json({ message: "Failed to access to db" });
     }
   }
-  static async getSingleUser(req, res){
+  static async getSingleUser(req, res) {
     try {
-      const singleUser = await Users.findOne({_id: req.params.id})
-      if(!singleUser){
+      const singleUser = await Users.findOne({ _id: req.params.id });
+      if (!singleUser) {
         return res.status(400).json({
-          status:"Fail",
-          message:"user with that Id does not exist!"
-        })
+          status: "Fail",
+          message: "user with that Id does not exist!",
+        });
       }
       return res.status(200).json({
-        status:"success",
-        message:"User Profile exist",
-        data:singleUser
-      })
+        status: "success",
+        message: "User Profile exist",
+        data: singleUser,
+      });
     } catch (error) {
       return res.status(500).json({
         status: "Fail",
-        message:error.message
-      })
+        message: error.message,
+      });
     }
   }
-  static async updateSingleUser(req, res){
+  static async updateSingleUser(req, res) {
     try {
-      const { idNo, firstName, lastName,phoneNumber,preferredContact,district,sector,cell} = req.body;
-      const userFound = await Users.findOne({_id: req.params.id})
-      if(idNo){
-        userFound.idNo=idNo;
+      const {
+        idNo,
+        firstName,
+        lastName,
+        phoneNumber,
+        preferredContact,
+        district,
+        sector,
+        cell,
+      } = req.body;
+      const userFound = await Users.findOne({ _id: req.params.id });
+      if (idNo) {
+        userFound.idNo = idNo;
       }
-      if(firstName){
-        userFound.firstName=firstName;
-      }if(lastName){
-        userFound.lastName=lastName;
-      }if(phoneNumber){
-        userFound.phoneNumber=phoneNumber;
-      }if(preferredContact){
-        userFound.preferredContact=preferredContact;
-      }if(district){
-        userFound.district=district;
-      }if(sector){
-        userFound.sector=sector;
-      }if(cell){
-        userFound.cell=cell;
+      if (firstName) {
+        userFound.firstName = firstName;
       }
-      userFound.updatedAt= new Date();
-      if(req.body.email){
+      if (lastName) {
+        userFound.lastName = lastName;
+      }
+      if (phoneNumber) {
+        userFound.phoneNumber = phoneNumber;
+      }
+      if (preferredContact) {
+        userFound.preferredContact = preferredContact;
+      }
+      if (district) {
+        userFound.district = district;
+      }
+      if (sector) {
+        userFound.sector = sector;
+      }
+      if (cell) {
+        userFound.cell = cell;
+      }
+      userFound.updatedAt = new Date();
+      if (req.body.email) {
         return res.status(400).json({
-          status:"bad request",
-          message:"email can not be updated"
-        })
+          status: "bad request",
+          message: "email can not be updated",
+        });
       }
-      await userFound.save()
+      await userFound.save();
       return res.status(200).json({
-        status:"sucess",
+        status: "sucess",
         message: "profile updated successfully",
-        data: userFound
-      })
-
+        data: userFound,
+      });
     } catch (error) {
       return res.status(500).json({
-        status: 'internal server error',
+        status: "internal server error",
         error: err.message,
       });
     }
   }
-  static async updateUserPassword(req,res){
+  static async updateUserPassword(req, res) {
     try {
-      const {oldPassword, newPassword} = req.body;
-      const userFound = await Users.findOne({_id:req.params.id})
-      if(oldPassword!==userFound.password){
+      const { oldPassword, newPassword } = req.body;
+      const userFound = await Users.findOne({ _id: req.params.id });
+      if (oldPassword !== userFound.password) {
         return res.status(400).json({
-          stautus:"Fail",
-          message:"old password is not correct"
-        })
+          stautus: "Fail",
+          message: "old password is not correct",
+        });
       }
       userFound.password = newPassword;
       await userFound.save();
-      return res.status(200).json({ data: userFound.password, message: "Password Updated successfully" });
+      return res.status(200).json({
+        data: userFound.password,
+        message: "Password Updated successfully",
+      });
     } catch (error) {
       return res.status(500).json({
         status: "fail",
-        message: error.message
+        message: error.message,
       });
     }
   }
   static async verifyEmail(req, res) {
     try {
       const { token } = req.params;
-  
+
       // Verify the JWT token
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-  
+
       // Extract email from the decoded token
       const { email } = decoded;
-  
+
       // Update the 'verified' column to true for the user with the provided email
       const user = await Users.findOneAndUpdate(
         { email },
         { $set: { verified: true } },
         { new: true }
       );
-  
+
       // Check if the user exists
       if (!user) {
-        return res.status(404).json({ error: "User not found" });
+        return res.status(404).json({
+          status:"fail",
+           message: "User not found" });
       }
-  
+
       // Send response indicating successful email verification
-      return res.status(200).json({ data: user, message: "Email verified successfully" });
+      return res
+        .status(200)
+        .json({ data: user,
+          status:"success",
+          message: "Email verified successfully" });
     } catch (error) {
       console.error("Error verifying email:", error);
       // Send internal server error response
-      return res.status(500).json({ error: "Internal server error" });
+      return res.status(500).json({ 
+        status:"fail",
+        error: "Internal server error" });
     }
   }
-  static async loginUser(req,res){
+  static async loginUser(req, res) {
     try {
-      const {email, password} = req.body;
+      const { email, password } = req.body;
 
-      const userFound = await Users.findOne({email})
-      if(!userFound){
+      const userFound = await Users.findOne({ email });
+      if (!userFound) {
         return res.status(404).json({
-          status:"fail",
-          message:"Account does not exist"
-        })
+          status: "fail",
+          message: "Account does not exist",
+        });
       }
-      
-      if(password!== userFound.password){
+
+      if (password !== userFound.password) {
         return res.status(400).json({
           status: "fail",
-          message: "Incorect credentials"
-        })
+          message: "Incorect credentials",
+        });
       }
-      if(userFound.verified !== true){
+      if (userFound.verified !== true) {
         return res.status(400).json({
-          status:"Fail",
-          message:"Your Acccount is not verified!🤷‍♀️"
-        })
+          status: "Fail",
+          message: "Your Acccount is not verified!🤷‍♀️",
+        });
       }
       const token = JWT.generateJwt({
         userId: userFound._id,
         role: userFound.role,
-        email:userFound.email,
+        email: userFound.email,
         firstName: userFound.firstName,
-        isernid: userFound.idNo
-      })
+        isernid: userFound.idNo,
+      });
       return res.status(200).json({
         status: "success",
         message: "Logged in successfully",
         token,
-        role:userFound.role,
-        user:{
-        userId: userFound._id,
         role: userFound.role,
-        email:userFound.email,
-        firstName: userFound.firstName,
-        isernid: userFound.idNo
-
-        }
-        
-      })
+        user: {
+          userId: userFound._id,
+          role: userFound.role,
+          email: userFound.email,
+          firstName: userFound.firstName,
+          isernid: userFound.idNo,
+        },
+      });
     } catch (error) {
       return res.status(500).json({
         status: "error",
-        message:error.message
-      })
+        message: error.message,
+      });
     }
   }
   static async resetPassword(req, res) {
@@ -274,16 +350,21 @@ class UsersController {
         return res.status(400).json({ message: "User not found" });
       }
 
-      const token = jwt.sign({ userId: findUser._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+      const token = jwt.sign({ userId: findUser._id }, process.env.JWT_SECRET, {
+        expiresIn: "1h",
+      });
       findUser.resetToken = token;
       findUser.resetTokenExpiration = Date.now() + 3600000; // 1 hour
       await findUser.save();
 
       console.log("Token generated:", token);
-      console.log("Token expiration:", new Date(findUser.resetTokenExpiration).toISOString());
+      console.log(
+        "Token expiration:",
+        new Date(findUser.resetTokenExpiration).toISOString()
+      );
 
       const transporter = nodemailer.createTransport({
-        service: 'gmail',
+        service: "gmail",
         auth: {
           user: process.env.EMAIL_USERNAME,
           pass: process.env.EMAIL_PASSWORD,
@@ -299,9 +380,8 @@ class UsersController {
 
       transporter.sendMail(mailOptions, (err, info) => {
         if (err) return res.status(500).send(err.toString());
-        res.status(200).send('Reset email sent');
+        res.status(200).send("Reset email sent");
       });
-
     } catch (error) {
       return res.status(500).json({ status: "error", message: error.message });
     }
@@ -323,7 +403,7 @@ class UsersController {
 
       if (!user) {
         console.log("Invalid or expired token");
-        return res.status(400).send('Invalid or expired token');
+        return res.status(400).send("Invalid or expired token");
       }
 
       // Hash the password before saving
@@ -333,10 +413,10 @@ class UsersController {
       user.resetTokenExpiration = undefined;
       await user.save();
 
-      res.status(200).send('Password reset successful');
+      res.status(200).send("Password reset successful");
     } catch (error) {
       console.log("Error verifying token:", error.message);
-      return res.status(400).send('Invalid or expired token');
+      return res.status(400).send("Invalid or expired token");
     }
   }
 }
